b0e260df09a49e4e13a78162cd3b1665f960f50e
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-27 15:44:20 -0400

    Import CountryList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import CountryList from './components/CountryList';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      if (data) {
+        setCountries(data);
+      } else {
+        console.log('Failed to retrieve countries from the API.');
+      }
+     } catch(error) {
+      console.error('An error occured while fetching countries:', error);
+     }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  useEffect(() => {
+    console.log(countries);
+  }, [countries]);
 
   return (
     <div className='container'>
-
+      <CountryList countries={countries} />
     </div>
   )
 }
