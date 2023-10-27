dadf91ca56009c26afa20f2806ffca73767f7fd4
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-27 17:03:33 -0400

    Add information to CountryCard

---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/CountryCard.css';
+
+function CountryCard({ country }) {
+
+    const formattedPopulation = country.population.toLocaleString();
+    return (
+        <div className="country-card">
+            <div className='card-top'><img src={country.flags.svg} alt='country flag' className='flag-image'/></div>
+            <div className='card-bottom'>
+                <div className='country-name'>{country.name.common}</div>
+                <div className='card-info-container'>
+                    <div className='country-population label'><span className='card-label'>Population:</span> {formattedPopulation}</div>
+                    <div className='country-region label'><span className='card-label'>Region:</span> {country.region}</div>
+                    <div className='country-capital label'><span className='card-label'>Capital:</span> {country.capital}</div>
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default CountryCard;
