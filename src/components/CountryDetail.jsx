75e8daf3ba61a85183328c5b18db1cd798632971
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-31 15:31:33 -0400

    Add CountryDetail info

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import '../styles/CountryDetail.css';
+import leftArrow from '../images/arrow-back-outline.svg';
 
 function CountryDetail() {
   const { cca3 } = useParams();
-
   const [countryData, setCountryData] = useState(null);
 
   useEffect(() => {
-    // Fetch data for the specific country using the cca3 code
     const fetchCountryData = async () => {
       try {
         const response = await fetch(`https://restcountries.com/v3.1/alpha/${cca3}`);
@@ -25,17 +25,63 @@ function CountryDetail() {
     fetchCountryData();
   }, [cca3]);
 
-  // Render the country details
   return (
     <div>
-      {countryData ? (
-        <div>
-          <h1>{countryData[0].name.common}</h1>
-          {/* Add more details here */}
-        </div>
-      ) : (
-        <p>Loading country data...</p>
-      )}
+        {countryData ? (
+            <div className='country-detail-container'>
+                <div className='country-detail-body'>
+                    <Link to="/" className='back-link'>
+                        <div className='left-arrow'>
+                            <img src={leftArrow} alt="left arrow" className='left-arrow-image' />
+                        </div> Back
+                    </Link>
+                    <div className='country-information-body'>
+                        <div className='country-information-left'>
+                            <img src={countryData[0].flags.svg} alt='country flag' className='country-detail-flag'></img>
+                        </div>
+                        <div className='country-information-right'>
+                            <div className='country-detail-name'>{countryData[0].name.common}</div>
+                            <div className='country-details'>
+                                <div className='country-details-left'>
+                                    <div className='country-details-section'><span className='country-details-title'>Native Name: </span><span className='country-details-info'>{countryData[0].altSpellings[1]}</span></div>
+                                    <div className='country-details-section'><span className='country-details-title'>Population: </span><span className='country-details-info'>{countryData[0].population ? countryData[0].population.toLocaleString() : 'N/A'}</span></div>
+                                    <div className='country-details-section'><span className='country-details-title'>Region: </span><span className='country-details-info'>{countryData[0].region}</span></div>
+                                    <div className='country-details-section'><span className='country-details-title'>Sub Region: </span><span className='country-details-info'>{countryData[0].subregion}</span></div>
+                                    <div className='country-details-section'><span className='country-details-title'>Capital: </span><span className='country-details-info'>{countryData[0].capital}</span></div>
+                                </div>
+                                <div className='country-details-right'>
+                                    <div className='country-details-section'><span className='country-details-title'>Top Level Domain: </span><span className='country-details-info'>{countryData[0].tld}</span></div>
+                                    <div className='country-details-section'>
+                                        <span className='country-details-title'>Currencies: </span>
+                                        <span className='country-details-info'>
+                                            {Object.entries(countryData[0].currencies).map(([code, currencyInfo]) => (
+                                            <span key={code}>
+                                                {currencyInfo.name}
+                                                {' '}
+                                            </span>
+                                            ))}
+                                        </span>
+                                    </div>
+                                    <div className='country-details-section'>
+                                        <span className='country-details-title'>Languages: </span>
+                                        <span className='country-details-info'>
+                                            {Object.values(countryData[0].languages).join(', ')}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='border-countries'>
+                                <div className='country-details-title'>Border Countries: </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ) : (
+            <div className='loading-container'>
+                <div className='loading-message'>Fetching Country Info...</div>
+            </div>
+        )}
     </div>
   );
 }
