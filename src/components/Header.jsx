71f7a41e67d0a253012dbc5026646f1114018e8f
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-02 13:49:28 -0400

    Add dark mode conditional rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
+import React, { useState } from 'react';
 import '../styles/Header.css';
 import moonIcon from '../images/moon-outline.svg';
 
-function Header() {
+function Header({ toggleDarkMode, darkMode }) {
+    
+
     return (
-        <div className='header-container'>  
+        <div className={`header-container ${darkMode ? 'dark-mode' : ''}`}>
             <div className='header-body'>
                 <div className='header-left'>Where in the world?</div>
-                <div className='header-right'>
+                <div className='header-right' onClick={toggleDarkMode}>
                     <div className='moon-icon-container'>
-                        <img src={moonIcon} alt='moon icon' className='moon-icon'/>
+                        <img src={moonIcon} alt='moon icon' className={`moon-icon ${darkMode ? 'white-icon' : ''}`}/>
                     </div>
-                    <div className='dark-mode'>Dark Mode</div>
+                    <div className='dark-mode-text'>{darkMode ? 'Light Mode' : 'Dark Mode'}</div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header;
